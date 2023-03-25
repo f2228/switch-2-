92e4033a5c446df544cfe1fc6a7b71b841588e77
f2228 <124640481+f2228@users.noreply.github.com>
2023-03-25 15:06:00 +0000

    Initial commit

---
+++ main.swift
@@ -0,0 +1,12 @@
+
+let wholeNumber = Int.random(in: 10...20)
+print("Generated number: \(wholeNumber)")
+
+switch wholeNumber {
+case let x where x % 2 == 0:
+    print("Composite")
+case let x where x % 3 == 0:
+    print("Composite")
+default:
+    print("Prime")
+}
